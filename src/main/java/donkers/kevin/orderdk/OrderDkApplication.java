cde8de7a25e99340a846e9115c2d3cbf58844ccb
DonkersKevin <donkers.kevin86@gmail.com>
2022-10-27 11:28:00 +0200

    Initial commit

---
+++ src/main/java/donkers/kevin/orderdk/OrderDkApplication.java
@@ -0,0 +1,13 @@
+package donkers.kevin.orderdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderDkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderDkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/donkers/kevin/orderdk/OrderDkApplicationTests.java
@@ -0,0 +1,13 @@
+package donkers.kevin.orderdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
